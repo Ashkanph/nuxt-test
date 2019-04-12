c1f97b8c686d1ebdc0b7f39cb1ebdfc9cbd6550b
Ashkan Paleganeh <ashkanpaleganeh@gmail.com>
2019-04-12 21:31:02 +0430

    Add UI components (AppButton, AppControlInput)

---
+++ components/UI/AppButton.vue
@@ -0,0 +1,72 @@
+<template>
+  <button
+    class="button"
+    :class="btnStyle"
+    v-bind="$attrs"
+    v-on="$listeners"><slot /></button>
+</template>
+
+<script>
+export default {
+  name: 'AppButton',
+  props: {
+    btnStyle: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.button {
+  font: inherit;
+  background-color: black;
+  color: #ccc;
+  padding: 5px;
+  cursor: pointer;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.button:hover,
+.button:active {
+  background-color: rgb(51, 51, 51);
+  border: 1px solid black;
+}
+
+.button_color_inverted {
+  background-color: white;
+  color: #6c757d;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.button_color_inverted:hover,
+.button_color_inverted:active {
+  color: #444;
+  background-color: white;
+  border: 1px solid #aaa;
+}
+
+.button_color_red {
+  background-color: red;
+  color: white;
+  border: 1px solid transparent;
+}
+
+.button_color_red:hover,
+.button_color_red:active {
+  background-color: salmon;
+  border: 1px solid transparent;
+  color: white;
+}
+
+.button:disabled,
+.button:disabled:hover{
+  color: #ccc;
+  border: 1px solid #ccc;
+  cursor: default;
+}
+</style>
---
+++ components/UI/AppControlInput.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="input-control">
+    <label class="input-control__label"><slot /></label>
+    <input
+      class="input-control__input"
+      v-if="controlType === 'input'"
+      v-bind="$attrs"
+      :value="value"
+      @input="$emit('input', $event.target.value)">
+    <textarea
+      class="input-control__textarea"
+      v-if="controlType === 'textarea'"
+      v-bind="$attrs"
+      :value="value"
+      @input="$emit('input', $event.target.value)"></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppInputControl',
+  props: {
+    controlType: {
+      type: String,
+      default: 'input'
+    },
+    value: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .input-control {
+    margin: 10px 0;
+  }
+
+  .input-control__label {
+    display: block;
+    font-weight: bold;
+  }
+
+  .input-control__input,
+  .input-control__textarea {
+    display: block;
+    width: 100%;
+    box-sizing: border-box;
+    font: inherit;
+    border: 1px solid #ccc;
+    padding: 5px;
+  }
+
+  .input-control__input:focus,
+  .input-control__textarea:focus {
+    background-color: #eee;
+    outline: none;
+  }
+</style>
+
+
